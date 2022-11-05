19465092fedfb2fcb4c17b543494f97534ce5b68
woodie91 <zoewoodroffe@hotmail.com>
2022-11-05 14:29:36 +0000

    comment out code

---
+++ Java.js
@@ -0,0 +1,16 @@
+let countEl = document.getElementById("count-el")
+let count = 0;
+
+function increment () {
+    count = count + 1;
+    countEl.innerText = count;
+}
+
+// function save() {
+    // console.log(count);
+// }
+
+
+// if (count > 9) {
+  //  alert ("You gone need a turlet hunty!")
+// } 
